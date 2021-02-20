bc2c22b2c3e879f9ee8a3d9e47cdc81dc0c3a48a
Rodolfo Barrios <rodolfo.barrios@avista.co>
2021-02-20 12:30:54 -0500

    Se crea una clase Dice para moldear un dato con lados que el programador pueda asignar a su ves, se modifica el main para poder darle accion al boton y que muestre el numero generado en el textView

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.diceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.diceroller.model.Dice
 
+/**
+ * Esta actividad permite cargar la vista principal y darle accion al boton de la misma
+ * para mostrar un numero aleatorio a partir de la clase dado que se le pasa el numero de
+ * lados en su constructor
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener { rollDice() }
     }
+
+    /**
+     * Este metodo inicializa el textView y genera un objeto de la clase Dice
+     * luego mediante su metodo roll se le setea el resultado al textView
+     */
+    private fun rollDice() {
+        val myDice = Dice(6)
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = myDice.roll().toString()
+    }
+
 }
---
+++ app/src/main/java/com/example/diceroller/model/Dice.kt
@@ -0,0 +1,9 @@
+package com.example.diceroller.model
+
+class Dice(private val numSides: Int) {
+
+    fun roll(): Int {
+        return (1..numSides).random()
+    }
+
+}
